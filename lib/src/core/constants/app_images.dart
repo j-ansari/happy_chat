338ac0ad2458f112dfb414120b20bed215994603
jansari <jansari.it@gmail.com>
2025-09-08 16:23:06 +0330

    change image to png

--- lib/src/core/constants/app_images.dart
+++ lib/src/core/constants/app_images.dart
@@ -1,5 +1,5 @@
 class AppImages {
   static const String backIcon = 'assets/images/back.svg';
-  static const String bg = 'assets/images/empty_bg.svg';
+  static const String bg = 'assets/images/empty_bg.png';
   static const String flag = 'assets/images/iran_flag.svg';
 }
