4269a77d25e249f1fe303d5dcdb849d7ca63e6f5
jansari <jansari.it@gmail.com>
2025-09-08 18:08:46 +0330

    remove un needed code

--- lib/src/view/widgets/custom_text_field.dart
+++ lib/src/view/widgets/custom_text_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:happy_chat_app/src/core/helper/context_extension.dart';
 import '../../core/helper/custom_regex_handler.dart';
 
-class CustomTextField extends StatefulWidget {
+class CustomTextField extends StatelessWidget {
   final TextEditingController? controller;
   final String? label;
   final Color? borderColor;
@@ -13,6 +13,7 @@ class CustomTextField extends StatefulWidget {
   final Widget? prefix;
   final ValueNotifier<bool>? focusNotifier;
   final String? Function(String?)? validator;
+  final double? padding;
 
   const CustomTextField({
     super.key,
@@ -26,74 +27,42 @@ class CustomTextField extends StatefulWidget {
     this.prefix,
     this.focusNotifier,
     this.validator,
+    this.padding,
   });
 
   @override
-  State<CustomTextField> createState() => _CustomTextFieldState();
-}
-
-class _CustomTextFieldState extends State<CustomTextField> {
-  late FocusNode _focusNode;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode = FocusNode();
-    _focusNode.addListener(() {
-      widget.focusNotifier?.value = _focusNode.hasFocus;
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final defColor = widget.borderColor ?? context.colorSchema.outline;
-    final currentBorderColor =
-        _focusNode.hasFocus ? context.colorSchema.outlineVariant : defColor;
-
-    final double leftPadding = widget.prefix != null ? 48 : 12;
+    final double leftPadding = prefix != null ? 48 : 12;
 
     return Stack(
       children: [
         TextFormField(
-          controller: widget.controller,
-          onChanged: widget.onChanged,
-          maxLength: widget.maxLength,
+          controller: controller,
+          onChanged: onChanged,
+          maxLength: maxLength,
           cursorColor: context.colorSchema.outlineVariant,
-          keyboardType: widget.keyboardType,
-          focusNode: _focusNode,
-          validator: widget.validator,
+          keyboardType: keyboardType,
+          validator: validator,
           decoration: InputDecoration(
             counter: const SizedBox.shrink(),
-            labelText: widget.label,
+            labelText: label,
             labelStyle: context.textTheme.labelMedium,
-            fillColor: widget.bgColor ?? Colors.white,
+            fillColor: bgColor ?? Colors.white,
             filled: true,
             contentPadding: EdgeInsets.fromLTRB(leftPadding, 12, 12, 12),
-            enabledBorder: _border(currentBorderColor),
+            enabledBorder: _border(borderColor ?? context.colorSchema.outline),
             errorBorder: _border(context.colorSchema.error),
             focusedBorder: _border(context.colorSchema.outlineVariant),
           ),
           inputFormatters: [
-            if (widget.keyboardType == TextInputType.number ||
-                widget.keyboardType == TextInputType.phone)
+            if (keyboardType == TextInputType.number ||
+                keyboardType == TextInputType.phone)
               CustomRegexHandler(regex: RegExp(r'^[0-9]+$')),
           ],
         ),
 
-        if (widget.prefix != null)
-          Positioned(
-            left: 0,
-            top: 0,
-            bottom: 0,
-            child: Center(child: widget.prefix),
-          ),
+        if (prefix != null)
+          Positioned(left: 0, top: 0, bottom: 0, child: Center(child: prefix)),
       ],
     );
   }
