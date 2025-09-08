fdf7d4a9d4c283db625d86d46b53add570604134
jansari <jansari.it@gmail.com>
2025-09-08 10:12:06 +0330

    add back icon

--- lib/src/core/constants/app_images.dart
+++ lib/src/core/constants/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
-  static const String flag = 'assets/images/iran_flag.svg';
+  static const String backIcon = 'assets/images/back.svg';
   static const String bg = 'assets/images/empty_bg.svg';
+  static const String flag = 'assets/images/iran_flag.svg';
 }
