919b7ebabe725a9f7625f6552d5f178439a975a1
jansari <jansari.it@gmail.com>
2025-09-08 16:23:27 +0330

    fix status bar color

--- lib/src/view/widgets/base_widget.dart
+++ lib/src/view/widgets/base_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:happy_chat_app/src/core/constants/app_images.dart';
 import 'package:happy_chat_app/src/core/helper/context_extension.dart';
@@ -25,75 +26,76 @@ class BaseWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar:
-            hasAppBar
-                ? AppBar(
-                  automaticallyImplyLeading: false,
-                  backgroundColor: Colors.transparent,
-                  title: Row(
-                    children: [
-                      hasSearch
-                          ? GestureDetector(
-                            onTap: () {},
-                            child: Icon(Icons.search),
-                          )
-                          : GestureDetector(
-                            onTap: () => Navigator.pop(context),
-                            child: Container(
-                              padding: const EdgeInsets.all(7),
-                              height: 28,
-                              width: 28,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: context.colorSchema.outline,
-                              ),
-                              child: SvgPicture.asset(AppImages.backIcon),
-                            ),
-                          ),
-                      const SizedBox(width: 16),
-                      if (!hasSearch)
-                        Text('بازگشت', style: context.textTheme.bodyMedium),
-                      const Spacer(),
-                      if (appBarTitle != null)
-                        Text(
-                          appBarTitle ?? '',
-                          style: context.textTheme.titleMedium,
-                        ),
-                      const Spacer(),
-                      if (hasAvatar)
-                        Icon(
-                          Icons.person_pin,
-                          size: 44,
-                          color: Colors.grey[600],
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar:
+          hasAppBar
+              ? AppBar(
+                systemOverlayStyle: SystemUiOverlayStyle(
+                  statusBarColor: Colors.white,
+                  statusBarIconBrightness: Brightness.dark,
+                  statusBarBrightness: Brightness.light,
+                ),
+                elevation: 0,
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.transparent,
+                title: Row(
+                  children: [
+                    hasSearch
+                        ? GestureDetector(
+                          onTap: () {},
+                          child: Icon(Icons.search),
                         )
-                      else
-                        const SizedBox(width: 44),
-                    ],
-                  ),
-                )
-                : null,
-        body: Container(
-          padding: EdgeInsets.all(padding),
-          width: context.screenWidth,
-          height: context.screenHeight,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: const [Colors.white, Colors.white, Color(0xfffcedea)],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              stops: [0.0, 0.2, 1],
-            ),
+                        : GestureDetector(
+                          onTap: () => Navigator.pop(context),
+                          child: Container(
+                            padding: const EdgeInsets.all(7),
+                            height: 28,
+                            width: 28,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: context.colorSchema.outline,
+                            ),
+                            child: SvgPicture.asset(AppImages.backIcon),
+                          ),
+                        ),
+                    const SizedBox(width: 16),
+                    if (!hasSearch)
+                      Text('بازگشت', style: context.textTheme.bodyMedium),
+                    const Spacer(),
+                    if (appBarTitle != null)
+                      Text(
+                        appBarTitle ?? '',
+                        style: context.textTheme.titleMedium,
+                      ),
+                    const Spacer(),
+                    if (hasAvatar)
+                      Icon(Icons.person_pin, size: 44, color: Colors.grey[600])
+                    else
+                      const SizedBox(width: 44),
+                  ],
+                ),
+              )
+              : null,
+      body: Container(
+        padding: EdgeInsets.all(padding),
+        width: context.screenWidth,
+        height: context.screenHeight,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: const [Colors.white, Colors.white, Color(0xfffcedea)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 0.2, 1],
           ),
-          child: body,
         ),
-        floatingActionButton: Padding(
-          padding: EdgeInsets.symmetric(horizontal: padding),
-          child: fab,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        child: body,
       ),
+      floatingActionButton: Padding(
+        padding: EdgeInsets.symmetric(horizontal: padding),
+        child: fab,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
