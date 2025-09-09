b25c6f17478a2098ac6584ff249f2312c18f3d0d
jansari <jansari.it@gmail.com>
2025-09-09 10:01:37 +0330

    create new var for save theme

--- lib/src/core/helper/prefs.dart
+++ lib/src/core/helper/prefs.dart
@@ -2,22 +2,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class PrefsKey {
   static const String token = "Token";
+  static const String isDark = "IsDark";
 }
 
 class Preferences {
   static late SharedPreferences preferences;
 
-  static bool? getBool(String key) {
-    return preferences.getBool(key);
-  }
+  static bool? getBool(String key) => preferences.getBool(key);
 
-  static String? getString(String key) {
-    return preferences.getString(key);
-  }
+  static String? getString(String key) => preferences.getString(key);
 
-  static Future<bool> remove(String key) {
-    return preferences.remove(key);
-  }
+  static Future<bool> remove(String key) => preferences.remove(key);
 
   static Future<bool> setBool(String key, bool value) {
     return preferences.setBool(key, value);
@@ -26,6 +21,8 @@ class Preferences {
   static Future<bool> setString(String key, String value) {
     return preferences.setString(key, value);
   }
+
+  static bool containsKey(String key) => preferences.containsKey(key);
 
   static final Preferences _instance = Preferences._internal();
 
